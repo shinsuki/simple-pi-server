4784c48f81320946ae19ced02e6fc8b650fd307d
shinsuki <36159060+shinsuki@users.noreply.github.com>
2018-02-12 14:08:14 +0900

    Input MESSAGE

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ SOME_API_KEY = os.getenv('SOME_API_KEY', None)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return MESSAGE
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
